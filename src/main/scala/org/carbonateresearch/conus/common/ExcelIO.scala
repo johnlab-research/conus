b247c826b709ede073beb47aaed4a2558cb3d1b2
Cedric John <cedric.john@imperial.ac.uk>
2020-05-19 13:35:26 +0100

    Universal Grid 1D now functional, including correct calculations. Uploaded final changes before going ahead and changing the grid so that variables are represented as the first vector on the timescale...

--- src/main/scala/org/carbonateresearch/conus/common/ExcelIO.scala
+++ src/main/scala/org/carbonateresearch/conus/common/ExcelIO.scala
@@ -1,12 +1,25 @@
 package org.carbonateresearch.conus.common
-import org.apache.poi.ss.usermodel.{WorkbookFactory, DataFormatter}
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.ss.usermodel.{DataFormatter, WorkbookFactory}
+import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 import java.io.{File, FileOutputStream}
+import java.text.SimpleDateFormat
+
 import scala.jdk.CollectionConverters
+import java.lang.System.lineSeparator
+import java.util.{Calendar, Date}
 
 case object ExcelIO{
-  def writeExcel(models: List[SingleModelResults],filename:String): Unit ={
-    models.foreach(m => writeIndividualModel(m,filename+"/Model"+m.ID+".xlsx"))
+
+  private val EOL = lineSeparator()
+
+  def writeExcel(models: List[SingleModelResults], pathName:String): Unit ={
+    val dateAndTime:String = new SimpleDateFormat("/yyyy-MM-dd-hh-mm-ss").format(new Date)
+    val fullPathName = pathName+dateAndTime
+    val dir = new File(fullPathName)
+    val successful = dir.mkdirs
+    if (successful) {
+      println("Writing results to folder '"+fullPathName+"'")
+    models.foreach(m => writeIndividualModel(m,fullPathName+"/Model"+m.ID+".xlsx"))}
   }
 
   private def writeIndividualModel(model:SingleModelResults,filename:String):Unit = {
@@ -15,9 +28,19 @@ case object ExcelIO{
 
     // Add a sheet to the workbook
     val sheet1 = workbook.createSheet("Model results")
-    val sheet2 = workbook.createSheet("Initial model parameters")
+    val sheet2:XSSFSheet = workbook.createSheet("Initial model parameters")
 
-    val row = sheet2.createRow(0)
+    formatInitialConditions(model,sheet2)
+    formatModelData(model,sheet1)
+
+    // Save the updated workbook as a new file
+    val fos = new FileOutputStream(filename)
+    workbook.write(fos)
+    workbook.close()
+  }
+
+  private def formatInitialConditions(model:SingleModelResults, icSheet:XSSFSheet)={
+    val row = icSheet.createRow(0)
     val cell1 = row.createCell(0)
     cell1.setAsActiveCell()
     cell1.setCellValue("PARAMETER")
@@ -28,28 +51,41 @@ case object ExcelIO{
     cell3.setAsActiveCell()
     cell3.setCellValue("UNIT")
 
-    val headers:List[CalculationParametersIOLabels] = model.theGrid.variableMap.keys.toList
-
-    val numberOfRows = headers.indices
+    val initialConditions:List[(CalculationParametersIOLabels,Any)] = model.initialConditions.map(ic => (ic.variable,ic.value))
+    val numberOfRows = initialConditions.indices
 
     numberOfRows.foreach(r => {
-    val row = sheet2.createRow(r+1)
-    val cell1 = row.createCell(0)
-    cell1.setAsActiveCell()
-      cell1.setCellValue(headers(r).toString())
+      val row = icSheet.createRow(r+1)
+      val cell1 = row.createCell(0)
+      cell1.setAsActiveCell()
+      cell1.setCellValue(initialConditions(r)._1.toString)
 
       val cell2 = row.createCell(1)
       cell2.setAsActiveCell()
-      cell2.setCellValue(model.initialConditions(r).toString)
+      cell2.setCellValue(initialConditions(r)._2.toString)
 
       val cell3 = row.createCell(2)
       cell3.setAsActiveCell()
-      cell3.setCellValue(headers(r).unitName)
+      cell3.setCellValue(initialConditions(r)._1.unitName)
     })
-    val filename2 = "Model"+model.ID+".xlsx"
-    // Save the updated workbook as a new file
-    val fos = new FileOutputStream(filename2)
-    workbook.write(fos)
-    workbook.close()
+  }
+
+  private def formatModelData(model:SingleModelResults, dataSheet:XSSFSheet):Unit ={
+    val rawString = model.theGrid.toString
+    val rows:Array[String] = rawString.split(EOL)
+    val nbRows:Int = rows.length
+    val data:Array[Array[String]] = rows.map(str => str.split(","))
+    println(data(0)(0).toString)
+    val rowIndex = rows.indices
+    val colIndex = data(0).indices
+
+    rowIndex.foreach(ri => {
+      val row = dataSheet.createRow(ri)
+      colIndex.foreach(ci => {
+        val cell = row.createCell(ci)
+        cell.setAsActiveCell()
+        cell.setCellValue(data(ri)(ci))
+      })
+    })
   }
 }
